71478e1b68ba687e928c36b383a357be70d3f0bb
Tobias Hammerschmidt <tobias-hammerschmidt@users.noreply.github.com>
2017-10-05 13:45:51 +0200

    generalize NET_HOST_PARAM to NET_PARAM

--- install.sh
+++ install.sh
@@ -27,9 +27,10 @@ if [ -n "$NAME" ] ; then
 fi
 
 mkdir -p "$HOST$DATADIR"
+rm -f "$HOST$DATADIR"/docker-run-opts
 
 HOSTNAME_PARAM=
-NET_HOST_PARAM=false
+NET_PARAM=
 PUBLISH_PARAM=false
 CAP_ADD_PARAM=
 IP_ADDRESS_PARAM=
@@ -49,8 +50,17 @@ while [[ "$#" -ne '0' ]] ; do
 			HOSTNAME_PARAM="${1#hostname=}"
 			shift
 			;;
+		net)
+		        shift
+			NET_PARAM="$1"
+			shift
+			;;
+		net=*)
+			NET_PARAM="${1#net=}"
+			shift
+			;;
 		net-host)
-			NET_HOST_PARAM=true
+			NET_PARAM="host"
 			shift
 			;;
 		publish)
@@ -88,15 +98,15 @@ if [ -n "$HOSTNAME_PARAM" ] ; then
 	echo "-h $HOSTNAME_PARAM" >> "$HOST$DATADIR"/docker-run-opts
 	echo "$HOSTNAME_PARAM" > "$HOST$DATADIR"/hostname
 	OPTS="-h $HOSTNAME_PARAM"
-elif [ -z "$NET_HOST_PARAM" ] ; then
+elif [[ "$NET_PARAM" != "host" ]] ; then
 	echo "Please specify the hostname for the server with --hostname parameter." >&2
 	echo "Usage: atomic install$NAME_PARAM $IMAGE --hostname FQDN.of.the.IPA.server" >&2
 	exit 1
 fi
 
-if $NET_HOST_PARAM ; then
-	echo "--net=host" >> "$HOST$DATADIR"/docker-run-opts
-	OPTS="$OPTS --net=host"
+if [ -n "$NET_PARAM" ] ; then
+	echo "--net=$NET_PARAM" >> "$HOST$DATADIR"/docker-run-opts
+	OPTS="$OPTS --net=$NET_PARAM"
 fi
 
 if $PUBLISH_PARAM ; then
