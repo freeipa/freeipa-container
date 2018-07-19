0e3c12886d188b31e010dbec36266c792b4a222a
Jan Pazdziora <jpazdziora@redhat.com>
2018-07-19 21:29:05 +0200

    Run tests with persistent volume.

--- tests/build-and-run.sh
+++ tests/build-and-run.sh
@@ -9,8 +9,10 @@ if ! grep -F "Dockerfile.$dockerfile" <( echo "$BUILD_DOCKERFILES" ) ; then
 fi
 
 docker build -t local/freeipa-server -f Dockerfile.$dockerfile .
+mkdir data
 docker run $privileged -h ipa.example.test \
 	--sysctl net.ipv6.conf.all.disable_ipv6=0 \
 	--tmpfs /run --tmpfs /tmp -v /dev/urandom:/dev/random:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+	-v $(pwd)/data:/data \
 	-e PASSWORD=Secret123 local/freeipa-server \
 	exit-on-finished -U -r EXAMPLE.TEST --setup-dns --no-forwarders --no-ntp
