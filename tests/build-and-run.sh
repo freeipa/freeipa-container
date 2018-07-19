80bbaf5399f54bdb0225753d59f7f5c56b9aeb58
Jan Pazdziora <jpazdziora@redhat.com>
2018-07-19 21:30:33 +0200

    Add tests for --external-ca workflow.

--- tests/build-and-run.sh
+++ tests/build-and-run.sh
@@ -15,4 +15,14 @@ docker run $privileged -h ipa.example.test \
 	--tmpfs /run --tmpfs /tmp -v /dev/urandom:/dev/random:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 	-v $(pwd)/data:/data \
 	-e PASSWORD=Secret123 local/freeipa-server \
-	exit-on-finished -U -r EXAMPLE.TEST --setup-dns --no-forwarders --no-ntp
+	exit-on-finished -U -r EXAMPLE.TEST --setup-dns --no-forwarders --no-ntp $ca
+if [ -n "$ca" ] ; then
+	sudo tests/generate-external-ca.sh data
+	docker run $privileged -h ipa.example.test \
+		--sysctl net.ipv6.conf.all.disable_ipv6=0 \
+		--tmpfs /run --tmpfs /tmp -v /dev/urandom:/dev/random:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+		-v $(pwd)/data:/data \
+		-e PASSWORD=Secret123 local/freeipa-server \
+		exit-on-finished -U -r EXAMPLE.TEST --setup-dns --no-forwarders --no-ntp \
+			--external-cert-file=/data/ipa.crt --external-cert-file=/data/ca.crt
+fi
---
+++ tests/generate-external-ca.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DATA="$1"
+NSSDB=/tmp/nssdb-$RANDOM
+mkdir -p $NSSDB
+rm -rf $NSSDB/*
+certutil -N -d $NSSDB --empty-password
+echo -e "y\n\n\n$RANDOM\n\n" | certutil -S -n "IPA ROOTCA certificate" -s "cn=CAcert" -x -t "CT,," -m 1000 -v 120 -d $NSSDB -z /etc/hostname -2 --keyUsage=certSigning --extSKID
+certutil -L -d $NSSDB -n 'IPA ROOTCA certificate' -a > "$DATA"/ca.crt
+echo -e "$RANDOM\nn\n" | certutil -C -m 2346 -i "$DATA"/ipa.csr -o "$DATA"/ipa.crt -c 'IPA ROOTCA certificate' -d $NSSDB -a --extSKID
