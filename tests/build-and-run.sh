1dd3df673f63b559df2b85b8b036f5b7dbb4f7b4
Jan Pazdziora <jpazdziora@redhat.com>
2018-07-20 15:20:30 +0200

    Test creating replicas in CI.

--- tests/build-and-run.sh
+++ tests/build-and-run.sh
@@ -42,9 +42,27 @@ echo "RUN uuidgen > /data-template/build-id" >> Dockerfile.$dockerfile
 echo data >> .dockerignore
 docker build -t local/freeipa-server -f Dockerfile.$dockerfile .
 date
-docker run $privileged -h ipa.example.test \
+( docker run $privileged --name freeipa-master -h ipa.example.test \
 	--sysctl net.ipv6.conf.all.disable_ipv6=0 \
 	--tmpfs /run --tmpfs /tmp -v /dev/urandom:/dev/random:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 	-v $(pwd)/data:/data:Z \
-	local/freeipa-server \
-	exit-on-finished
+	local/freeipa-server | tee /tmp/freeipa-master.log ) &
+(
+set +x
+while ! grep -q 'FreeIPA server started' /tmp/freeipa-master.log ; do
+	sleep 10
+done
+)
+docker ps
+MASTER_IP=$( docker inspect --format '{{ .NetworkSettings.IPAddress }}' freeipa-master )
+date
+mkdir data-replica
+docker run $privileged --name freeipa-replica -h replica.example.test \
+	--sysctl net.ipv6.conf.all.disable_ipv6=0 \
+	--tmpfs /run --tmpfs /tmp -v /dev/urandom:/dev/random:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+	-v $(pwd)/data-replica:/data:Z \
+	--link freeipa-master:ipa.example.test --dns=$MASTER_IP \
+	-e PASSWORD=Secret123 local/freeipa-server \
+	exit-on-finished ipa-replica-install -U --skip-conncheck --principal admin --setup-ca --no-ntp
+docker ps
+date
