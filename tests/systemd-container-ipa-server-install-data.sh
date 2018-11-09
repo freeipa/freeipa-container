8a250f9190be89b00852e281fc16a37b38201493
Jan Pazdziora <jpazdziora@redhat.com>
2018-11-09 07:18:26 +0100

    Replace volume-data-mv-list approach with direct writes to /data.
    For temporary content, use the /tmp volume.
    
    Also add separate build step test for the /data-based setup.

--- tests/systemd-container-ipa-server-install-data.sh
+++ tests/systemd-container-ipa-server-install-data.sh
@@ -4,7 +4,7 @@ set -e
 # set -x
 
 C="$1"
-shift
+D="$2"
 
 if ! docker exec $C ipa-server-install -U -r EXAMPLE.TEST -p Secret123 -a Secret123 --setup-dns --no-forwarders --no-ntp ; then
 	FAILED=$( docker exec $C systemctl list-units --state=failed --no-pager -l --no-legend | tee /dev/stderr | sed 's/ .*//' | sort )
@@ -13,8 +13,20 @@ if ! docker exec $C ipa-server-install -U -r EXAMPLE.TEST -p Secret123 -a Secret
 	done
 	exit 1
 fi
+echo Secret123 | docker exec -i $C kinit admin
+docker exec $C ipa user-add --first Bob --last Nowak bob
+docker exec $C id bob
+
 MACHINE_ID=$( docker exec $C cat /etc/machine-id )
-docker exec $C ls -la /var/log/journal/$MACHINE_ID/system.journal /var/log/ipaserver-install.log
+docker exec $C ls -la /data/var/log/journal/$MACHINE_ID/system.journal /data/var/log/ipaserver-install.log
+if [ -z "$TRAVIS" ] ; then
+	ls -la /var/log/journal/$MACHINE_ID
+	if test -f /var/log/journal/$MACHINE_ID/system.journal ; then
+		exit 1
+	fi
+fi
+
+docker diff $C | tee /dev/stderr | sort | diff tests/$D /dev/stdin
 
 echo OK $0.
 
