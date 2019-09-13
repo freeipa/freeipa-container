cf3c78dcade89c7c398f98ad0e499a03bc4baf09
Jan Pazdziora <jpazdziora@redhat.com>
2019-09-13 09:43:02 +0200

    Do not expose ports 7389, 9443, 9444, and 9445 that were only needed on RHEL 6.

--- install.sh
+++ install.sh
@@ -110,8 +110,8 @@ if [ -n "$NET_PARAM" ] ; then
 fi
 
 if $PUBLISH_PARAM ; then
-	echo "-p 53:53 -p 80:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 -p 7389:7389 -p 9443:9443 -p 9444:9444 -p 9445:9445 -p 53:53/udp -p 88:88/udp -p 464:464/udp" >> "$HOST$DATADIR"/docker-run-opts
-	OPTS="$OPTS -p 53:53 -p 80:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 -p 7389:7389 -p 9443:9443 -p 9444:9444 -p 9445:9445 -p 53:53/udp -p 88:88/udp -p 464:464/udp"
+	echo "-p 53:53 -p 80:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 -p 53:53/udp -p 88:88/udp -p 464:464/udp" >> "$HOST$DATADIR"/docker-run-opts
+	OPTS="$OPTS -p 53:53 -p 80:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 -p 53:53/udp -p 88:88/udp -p 464:464/udp"
 fi
 
 while [ -n "$CAP_ADD_PARAM" ] ; do
