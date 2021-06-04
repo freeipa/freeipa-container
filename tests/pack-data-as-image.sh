0ed1c082407ecb93f60bf0147fe8040fce4d8aa6
Jan Pazdziora <jpazdziora@redhat.com>
2021-06-04 07:40:41 +0200

    Test upgrades from Fedora 34 container.

--- tests/pack-data-as-image.sh
+++ tests/pack-data-as-image.sh
@@ -6,11 +6,14 @@ set -x
 #
 # Example of preparing data image for upgrade testing:
 # mkdir -p freeipa-server/data
-# replica=none VOLUME=$(pwd)/freeipa-server/data tests/run-master-and-replica.sh freeipa/freeipa-server:fedora-33
+# replica=none VOLUME=$(pwd)/freeipa-server/data tests/run-master-and-replica.sh freeipa/freeipa-server:fedora-34
 # docker rm -f freeipa-master
-# sudo tests/pack-data-as-image.sh freeipa-server data-fedora-33
-# docker login ...
-# docker push freeipa/freeipa-server:data-fedora-33
+# sudo tests/pack-data-as-image.sh freeipa-server data-fedora-34
+# docker login index.docker.io
+# docker push freeipa/freeipa-server:data-fedora-34
+# docker tag freeipa/freeipa-server:data-fedora-34 quay.io/freeipa/freeipa-server:data-fedora-34
+# docker login quay.io
+# docker push quay.io/freeipa/freeipa-server:data-fedora-34
 #
 
 cd "$1"
@@ -18,15 +21,22 @@ tar cf data.tar data
 SUM=$( sha256sum data.tar )
 SUM=${SUM%% *}
 mv data.tar $SUM.tar
+DATE=$( TZ=Z date --iso-8601=ns | sed 's/,/./; s/+00:00/Z/' )
 cat <<EOF > config.json
 {
-  "created": "$( TZ=Z date --iso-8601=ns | sed 's/,/./; s/+00:00/Z/' )",
+  "created": "$DATE",
   "architecture": "amd64",
   "os": "linux",
   "rootfs": {
     "type": "layers",
     "diff_ids": [ "sha256:$SUM" ]
-  }
+  },
+  "history": [
+    {
+      "created": "$DATE",
+      "created_by": "/bin/sh -c #(nop) DATA VOLUME"
+    }
+  ]
 }
 EOF
 CSUM=$( sha256sum config.json )
