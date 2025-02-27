1f4b06809d11add95d53e264d9d613a340af68a7
Jan Pazdziora <jan.pazdziora@code.adelton.com>
2025-02-27 10:32:23 +0100

    Do not disable the /etc/resolv.conf / systemd-resolved setup, use resolvectl to configure the DNS server for cluster.local and custom forward configuration of coredns for the FreeIPA domain.

--- tests/run-master-in-k3s.sh
+++ tests/run-master-in-k3s.sh
@@ -25,6 +25,12 @@ kubectl get pods --all-namespaces
 sudo mkdir -p /var/lib/rancher/k3s/storage
 sudo chown $( id -u ) /var/lib/rancher/k3s/storage
 
+( set +x ; for i in $( seq 1 10 ) ; do ip link show cni0 > /dev/null && break ; sleep 3 ; done )
+sudo resolvectl dns cni0 $( kubectl get -n kube-system service/kube-dns -o 'jsonpath={.spec.clusterIP}' )
+sudo resolvectl domain cni0 cluster.local
+
+kubectl apply -f tests/freeipa-coredns-custom.yaml
+
 kubectl create -f <( sed "s#image:.*#image: $1#" tests/freeipa-k3s.yaml )
 ( set +x ; while kubectl get pod/freeipa-server | tee /dev/stderr | grep -Eq '\bPending\b|\bContainerCreating\b' ; do sleep 5 ; done )
 if ! kubectl get pod/freeipa-server | grep -q '\bRunning\b' ; then
@@ -41,18 +47,13 @@ kubectl describe pod/freeipa-server
 PV_DIR=$( kubectl get pvc/freeipa-data-pvc -o 'jsonpath={.spec.volumeName}_{.metadata.namespace}_{.metadata.name}' )
 ls -la /var/lib/rancher/k3s/storage/$PV_DIR
 IPA_SERVER_HOSTNAME=$( kubectl exec pod/freeipa-server -- hostname -f )
-if ! test -f /etc/resolv.conf.backup ; then
-	sudo mv /etc/resolv.conf /etc/resolv.conf.backup
-fi
-sudo systemctl stop systemd-resolved.service || :
 IPA_SERVER_IP=$( kubectl get -o=jsonpath='{.spec.clusterIP}' service freeipa-server-service )
-echo nameserver $IPA_SERVER_IP | sudo tee /etc/resolv.conf
+seq 15 -1 0 | while read i ; do dig +short $IPA_SERVER_HOSTNAME | tee /dev/stderr | grep -Fq $IPA_SERVER_IP && break ; sleep 5 ; [ $i == 0 ] && false ; done
+seq 15 -1 0 | while read i ; do dig +short -t srv _ldap._tcp.${IPA_SERVER_HOSTNAME#*.} | tee /dev/stderr | grep -Fq "0 100 389 $IPA_SERVER_HOSTNAME." && break ; sleep 5 ; [ $i == 0 ] && false ; done
+
 curl -Lk https://$IPA_SERVER_HOSTNAME/ | grep -E 'IPA: Identity Policy Audit|Identity Management'
 curl -H "Referer: https://$IPA_SERVER_HOSTNAME/ipa/ui/" -H 'Accept-Language: fr' -d '{"method":"i18n_messages","params":[[],{}]}' -k https://$IPA_SERVER_HOSTNAME/ipa/i18n_messages | grep -q utilisateur
 echo Secret123 | kubectl exec -i pod/freeipa-server -- kinit admin
-
-seq 15 -1 0 | while read i ; do dig +short $IPA_SERVER_HOSTNAME | tee /dev/stderr | grep -Fq $IPA_SERVER_IP && break ; sleep 5 ; [ $i == 0 ] && false ; done
-seq 15 -1 0 | while read i ; do dig +short -t srv _ldap._tcp.${IPA_SERVER_HOSTNAME#*.} | tee /dev/stderr | grep -Fq "0 100 389 $IPA_SERVER_HOSTNAME." && break ; sleep 5 ; [ $i == 0 ] && false ; done
 
 kill $MASTER_LOGS_PID 2> /dev/null || :
 trap - EXIT
@@ -73,11 +74,13 @@ kubectl describe pod/freeipa-replica
 PV_DIR=$( kubectl get pvc/freeipa-replica-pvc -o 'jsonpath={.spec.volumeName}_{.metadata.namespace}_{.metadata.name}' )
 ls -la /var/lib/rancher/k3s/storage/$PV_DIR
 IPA_REPLICA_HOSTNAME=$( kubectl exec pod/freeipa-replica -- hostname -f )
+IPA_REPLICA_IP=$( kubectl get -o=jsonpath='{.spec.clusterIP}' service freeipa-replica-service )
+dig +short $IPA_REPLICA_HOSTNAME | tee /dev/stderr | grep -Fq $IPA_REPLICA_IP
+
 curl -Lk https://$IPA_REPLICA_HOSTNAME/ | grep -E 'IPA: Identity Policy Audit|Identity Management'
 curl -H "Referer: https://$IPA_REPLICA_HOSTNAME/ipa/ui/" -H 'Accept-Language: fr' -d '{"method":"i18n_messages","params":[[],{}]}' -k https://$IPA_REPLICA_HOSTNAME/ipa/i18n_messages | grep -q utilisateur
 echo Secret123 | kubectl exec -i pod/freeipa-replica -- kinit admin
-IPA_REPLICA_IP=$( kubectl get -o=jsonpath='{.spec.clusterIP}' service freeipa-replica-service )
-dig +short $IPA_REPLICA_HOSTNAME | tee /dev/stderr | grep -Fq $IPA_REPLICA_IP
+
 kill $REPLICA_LOGS_PID 2> /dev/null || :
 trap - EXIT
 
